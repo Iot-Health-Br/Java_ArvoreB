cb52fc5d8d2c82389fa94d550710b340d70bb6d1
Iot-Health-Br <igl_macedo@hotmail.com>
2023-10-08 16:09:00 -0300

    Feito o comentario do código encontra-se 95% comentado, falta a divisão das classes inserção e delete para deixar o código da classe main organizado

--- src/Arvore_B/ArvoreB.java
+++ src/Arvore_B/ArvoreB.java
@@ -216,62 +216,105 @@ public class ArvoreB {
             removido da subárvore de `y`.*/
             Delete(y, pred);} 
         
-        else if (z.n >= t) {  // Caso 2b
-            /*localiza o sucessor de `k` no subárvore cuja raiz é `z`. O sucessor é a menor chave na subárvore de `z`.
-*/
+        else if (z.n >= t){  // Caso 2b
+            /*localiza o sucessor de `k` no subárvore cuja raiz é `z`. O sucessor é a menor chave na 
+            subárvore de `z`.*/
             int succ = findSuccessor(z);
+            // A chave `k` em `x` é substituída pelo seu sucessor.
             x.keys[idx] = succ;
+            // O sucessor original deve ser removido da subárvore de `z`.
             Delete(z, succ);} 
-        else {  // Caso 2c
+        else {  // Caso 2c - Se ambos `y` e `z` tiverem `t-1` chaves:
+            /* Combina os nós `Y` e `Z`, colocando a chave `K` do nó `X` entre eles. Após a mesclagem, 
+            o nó `Y` contém todas as chaves e filhos de `Z`, e o nó `Z` provavelmente é descartado.*/
             merge(x, idx);
+            /*Após a mesclagem, a chave `k` ainda precisa ser removida da subárvore cuja raiz 
+            é agora o nó `y` expandido.*/
             Delete(y, k);}
         return;}
 
-        // Se a chave não está em x.
+        // Se a chave não está em x. 
+        // Identifica o filho de x que pode conter a chave k.
         Node child = x.children[idx];
+        /* Se esse filho tiver menos de t chaves, o método fill é chamado para garantir que o filho 
+        tenha pelo menos t chaves antes de continuar com a operação de deleção. Este passo é crucial 
+        para garantir que a operação de deleção não violará as propriedades da árvore B.*/
         if (child.n < t) {
             fill(x, idx);}
+        /* Uma vez que o filho tem o número suficiente de chaves, a operação de deleção é chamada 
+        recursivamente neste filho.*/
         Delete(x.children[idx], k);
     }
-    
+    /* Encontrar o predecessor de uma chave, que é a maior chave na subárvore à esquerda dessa chave.*/
     private int findPredecessor(Node x) {
+        /*Enquanto x não for um nó folha, move-se para o filho mais à direita de x. 
+        O índice [x.n] aponta para o filho mais à direita, porque x.n é o número atual 
+        de chaves no nó, e a posição do filho mais à direita é exatamente essa.*/
         while (!x.leaf) {
             x = x.children[x.n];}
+        // Uma vez que alcançamos uma folha, o predecessor é a última chave no nó folha, que é x.keys[x.n - 1].
         return x.keys[x.n - 1];}
-
+    
+    /*Encontrar o sucessor de uma chave, que é a menor chave na subárvore à direita dessa chave.*/
     private int findSuccessor(Node x) {
+        /* Enquanto x não for um nó folha, move-se para o filho mais à esquerda de x. Isso é feito 
+        acessando o filho na posição [0], que é sempre o filho mais à esquerda.*/
         while (!x.leaf) {
             x = x.children[0];}
+        // Uma vez que alcançamos uma folha, o sucessor é a primeira chave no nó folha, que é 
         return x.keys[0];}
     
+    /* O método merge é responsável por combinar dois nós adjacentes de uma árvore B. 
+    Mescla um filho Y na posição IDX de um nó X com o próximo filho Z (na posição IDX + 1)*/
     private void merge(Node x, int idx) {
+        // Dois nós filhos de x que serão mesclados, y e z.
         Node y = x.children[idx];
         Node z = x.children[idx + 1];
-
+        
+        // A chave na posição idx de x é movida para a última posição do nó y
         y.keys[t - 1] = x.keys[idx];
 
+        /* Todas as chaves do nó z são copiadas para o nó y, começando logo após 
+        a chave recém-adicionada.*/
         for (int j = 0; j < z.n; j++) {
             y.keys[t + j] = z.keys[j];}
-
+        // Se z não for uma folha, ele terá filhos que precisam ser transferidos para y.
         if (!z.leaf) {
+            // Os filhos de z são copiados para y.
             for (int j = 0; j <= z.n; j++) {
                 y.children[t + j] = z.children[j];}}
-
+        
+        /* Todas as chaves após a posição idx em x são deslocadas para a esquerda para 
+        preencher a posição da chave que foi movida para y.*/
         for (int j = idx; j < x.n - 1; j++) {
             x.keys[j] = x.keys[j + 1];}
 
+        /*Semelhante às chaves, os ponteiros de filho são também deslocados para a esquerda.
+        Isso efetivamente remove o ponteiro para o nó z.*/
         for (int j = idx + 1; j < x.n; j++) {
             x.children[j] = x.children[j + 1];}
 
+        /* Atualiza o contador de chaves de y para refletir a adição das chaves de Z e a 
+        chave que desceu de X.*/
         y.n += z.n + 1;
+        // Uma chave foi removida de x, decrementamos o contador de chaves.
         x.n--;
     }
     
+    /* Esse método é usado durante a operação de deleção em uma árvore B para assegurar que 
+    qualquer nó (exceto a raiz) não caia abaixo do mínimo permitido de chaves. */
     private void fill(Node x, int idx) {
+        /*Verifica se existe um irmão à esquerda (anterior) e se ele tem mais do que o mínimo de
+        chaves permitidas (t).Se for verdadeiro, então é possível "emprestar" uma chave desse
+        irmão anterior. O método borrowFromPrev é chamado para realizar esse empréstimo*/
         if (idx > 0 && x.children[idx - 1].n >= t) {
             borrowFromPrev(x, idx);} 
+        /*Se o nó não puder emprestar do irmão anterior, verifica se há um irmão à direita (seguinte)
+        e se ele tem mais do que o mínimo de chaves permitidas.Se for esse o caso, então uma chave pode 
+        ser emprestada desse irmão. O método borrowFromNext é chamado para realizar esse empréstimo.*/
         else if (idx < x.n && x.children[idx + 1].n >= t) {
             borrowFromNext(x, idx);} 
+        /*Se o nó não puder emprestar de nenhum dos irmãos, então ele deve ser mesclado com um irmão.*/
         else {
             if (idx < x.n){
                 merge(x, idx);} 
@@ -279,6 +322,8 @@ public class ArvoreB {
                 merge(x, idx - 1);}}
     }
     
+    /*O método empresta uma chave do irmão anterior (à esquerda) quando um determinado nó 
+   (child) tem menos do que o mínimo permitido de chaves em uma árvore B.*/
     private void borrowFromPrev(Node x, int idx) {
         Node child = x.children[idx];
         Node sibling = x.children[idx - 1];
@@ -306,6 +351,9 @@ public class ArvoreB {
         sibling.n -= 1;
     }
     
+    /*O método empresta uma chave do irmão subsequente (à direita) 
+    quando um determinado nó (child) tem menos do que o mínimo permitido 
+    de chaves em uma árvore B.*/
     private void borrowFromNext(Node x, int idx) {
         Node child = x.children[idx];
         Node sibling = x.children[idx + 1];
@@ -334,11 +382,17 @@ public class ArvoreB {
     }
 
     public static void main(String[] args) {
+        // Objeto é usado para ler a entrada do usuário.
         Scanner scanner = new Scanner(System.in);
+        // Interação com o usuário
         System.out.println("Digite o valor de t:");
+        // Armazena o valor digitado pelo o usuário
         int t = scanner.nextInt();
+        // Uma nova arvore e criada através do atribuo T digitado pelo o usuário
         ArvoreB tree = new ArvoreB(t);
 
+        /* Loop infinito que continuará a executar até que seja interrompido por
+        algum meio, neste caso, pela opção 4 (Sair).*/
         while (true) {
             System.out.println("1: Inserir\n2: Excluir\n3: Imprimir\n4: Sair");
             int option = scanner.nextInt();
@@ -354,7 +408,7 @@ public class ArvoreB {
             
             else if (option == 3) {
                 tree.printTree(tree.root, 0);} 
-            else {
+            else if (option == 4){
                 break;}
         }
         scanner.close();
